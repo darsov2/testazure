21d18aff2c4945f759e9c63e3309caad55dec135
darsov2 <62809499+darsov2@users.noreply.github.com>
2024-09-17 20:55:44 +0200

    Update TestController.java

--- src/main/java/mk/ukim/finki/testazure/TestController.java
+++ src/main/java/mk/ukim/finki/testazure/TestController.java
@@ -14,6 +14,6 @@ public class TestController {
 
     @GetMapping
     public void david() {
-        System.out.println("test      " + test);
+        System.out.println("test       " + test);
     }
 }
