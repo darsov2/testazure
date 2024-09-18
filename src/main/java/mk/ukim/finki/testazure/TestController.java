4227d5a897633032211f27a7cd732bb53f79b70b
darsov2 <62809499+darsov2@users.noreply.github.com>
2024-09-18 07:22:19 +0200

    Update TestController.java

--- src/main/java/mk/ukim/finki/testazure/TestController.java
+++ src/main/java/mk/ukim/finki/testazure/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+    @Value("${david.test-azure-secrets.david}")
     String test;
 
     @GetMapping
